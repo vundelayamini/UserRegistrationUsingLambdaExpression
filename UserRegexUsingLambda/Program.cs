35854b4bef6fa6872f933b0e1a0466258dd1ba59
vundelayamini <vundelayamini@gmail.com>
2021-04-29 19:44:00 +0530

    [yamini]Add: Welcome Message to UserRegex Using Lambda Expression

---
+++ UserRegexUsingLambda/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegexUsingLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Message to User Regex Using Lambda");
+        }
+    }
+}
